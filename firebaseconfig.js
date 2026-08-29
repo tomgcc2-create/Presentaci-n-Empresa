773b60ab1469e4cf96a6c113960b5ff0c3774c27
isabelllasarabanda-arch <isabelllasarabanda@gmail.com>
2026-08-29 11:51:40 -0500

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { initializeApp } from "https://www..com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.cgstaticom/firebasejs/11.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 export const firebaseConfig = {
